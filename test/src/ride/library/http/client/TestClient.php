696d8f27377e7efe270e1687a4d68ed494b6b8f7
vagrant <vagrant@php56.puphpet>
2016-09-06 17:17:45 +0000

    AbstractClientTest kickoff

---
+++ test/src/ride/library/http/client/TestClient.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ride\library\http\client;
+
+use ride\library\http\Request;
+
+class TestClient extends AbstractClient {
+
+    public function sendRequest(Request $request) {
+    // TODO: Implement sendRequest() method.
+    }
+
+}
